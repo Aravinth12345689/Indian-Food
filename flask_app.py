f592148313317525a2924e58d02bcec20b162a3c
Aravinth12345689 <146167657+Aravinth12345689@users.noreply.github.com>
2026-07-23 14:23:48 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
-import os
 import json
 import pandas as pd
   
